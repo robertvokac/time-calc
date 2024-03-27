68970bf04e612eee7396e16cf8b55197553391ef
Robert Vokac <robertvokac@nanoboot.org>
2024-03-27 17:41:16 +0100

    Fixed antialiasing rendering

--- modules/time-calc-app/src/main/java/org/nanoboot/utils/timecalc/swing/common/Widget.java
+++ modules/time-calc-app/src/main/java/org/nanoboot/utils/timecalc/swing/common/Widget.java
@@ -22,6 +22,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -239,6 +240,10 @@ public class Widget extends JPanel implements
     @Override
     public final void paintComponent(Graphics brush) {
         super.paintComponent(brush);
+
+        ((Graphics2D)brush).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
         setVisible(visibleProperty.isEnabled());
         Visibility visibility
                 = Visibility.valueOf(visibilityProperty.getValue());
